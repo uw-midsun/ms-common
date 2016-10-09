49f99a040deaa3568c5304f619ba15403bee4259
ckitagawa <calderk2010@gmail.com>
2016-10-09 14:48:28 -0400

    Altered more names... Added a function declaration for setting state, clarified struct memory.

--- inc/gpio.h
+++ inc/gpio.h
@@ -2,16 +2,20 @@
 
 // GPIO HAL Interface
 
-#include <stdbool.h>
 #include <stdint.h>
 
 // GPIO Pin "address" to be used to change that pin's settings
 // Use bytes instead of words to save on memory also convenient for MSP430
 // compatibility
 typedef struct GPIOAddress {
-  const uint8_t gpio_port;
-  const uint8_t gpio_pin;
+  const uint8_t port;
+  const uint8_t pin;
 } GPIOAddress;
+
+// Enums size varies by compiler only garuntee is that they will be big enough
+// to store their defined number of items. For this particular case since they
+// are small they are likely (although far from certain) that they will be
+// sizeof(int) unless a --short-enums flag is passed to the compiler
 
 // For setting the direction of the pin
 typedef enum {
@@ -30,7 +34,7 @@ typedef enum {
 // set PIN_REN to 1. Could make this RES_ON, RES_OFF and make pullup and
 // pulldown vary with the PIN_VAL the same way it works for the MSP430
 typedef enum {
-  PIN_RES_OFF = 0,
+  PIN_RES_NONE = 0,
   PIN_RES_PULLUP,
   PIN_RES_PULLDOWN,
 } PinRes;
@@ -58,25 +62,26 @@ typedef enum {
 // input or wired OR gates
 // Reset and Lock setting on STM
 
-// A 3 byte struct to set the values for a pin
-// Could separate out the address from the settings
+// Address is separated to save memory and make the interface cleaner
+// Struct is 4 bytes and will fit in one memory register.
 typedef struct GPIOSettings {
-  const GPIOAddress addr;  // 2 bytes
-  PinDir dir;              // 1 bit
-  PinState state;          // 1 bit
-  PinRes res;              // 2 bits (one unused option)
-  PinAF alt;  // 4 bits (no padding needed if the compiler is smart about enums
-              // 7 unused options)
+  PinDir dir;
+  PinState state;
+  PinRes res;
+  PinAF alt;
 } GPIOSettings;
 
 // Initializes GPIO globally, sets all pins to lowest power mode
 void gpio_init();
 
 // Initializes a GPIO pin
-void gpio_init_pin(GPIOSettings *settings);
+void gpio_init_pin(GPIOAddress *address, GPIOSettings *settings);
+
+// Set the pin by address
+void gpio_set_pin_state(GPIOAddress *address, PinState *state);
 
 // Toggles the output state of the pin
 void gpio_toggle_state(GPIOAddress *address);
 
 // Gets the value of the input register for a pin
-bool gpio_get_value(GPIOAddress *address);
+PinState gpio_get_value(GPIOAddress *address);
