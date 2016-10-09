83b8e3f93cd66b340fc0e465980057f337bacbf3
ckitagawa <calderk2010@gmail.com>
2016-10-09 13:31:29 -0400

    Fixed naming simplified number of functions, looked at strcut packing

--- inc/gpio.h
+++ inc/gpio.h
@@ -5,41 +5,52 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-// IO Pin "address" to be used to change that pin's settings
+// GPIO Pin "address" to be used to change that pin's settings
 // Use bytes instead of words to save on memory also convenient for MSP430
 // compatibility
-typedef struct IOAddr {
-  const uint8_t IOPort;  // Could be an enum (A-F) for stm32f0xx
-  const uint8_t IOPin;
-} IOAddr;
+typedef struct GPIOAddress {
+  const uint8_t gpio_port;
+  const uint8_t gpio_pin;
+} GPIOAddress;
 
 // For setting the direction of the pin
-typedef enum { PIN_IN = 0; PIN_OUT; }
-PIN_DIR;
+typedef enum {
+  PIN_DIR_IN = 0,
+  PIN_DIR_OUT,
+} PIN_DIR;
 
 // For setting the output value of the pin
-typedef enum { PIN_LOW = 0; PIN_HIGH; }
-PIN_VAL;
+typedef enum {
+  PIN_STATE_LOW = 0,
+  PIN_STATE_HIGH,
+} PIN_STATE;
 
 // For setting the internal pull-up/pull-down resistor (Turn off if a hardware
-// PUPDR is enabled)
-// Different on MSP and STM for MSP RES_UP and RES_DOWN both set PIN_REN to 1
-// Could make this RES_ON, RES_OFF and make pullup and pulldown vary with the
-// PIN_VAL as it works
-// for the MSP430
-typedef enum { RES_OFF = 0; RES_UP; RES_DOWN; }
-PIN_RES;
+// PUPDR is enabled). Different on MSP and STM for MSP RES_UP and RES_DOWN both
+// set PIN_REN to 1. Could make this RES_ON, RES_OFF and make pullup and
+// pulldown vary with the PIN_VAL the same way it works for the MSP430
+typedef enum {
+  PIN_RES_OFF = 0,
+  PIN_RES_PULLUP,
+  PIN_RES_PULLDOWN,
+} PIN_RES;
 
 // For setting the alternate function on the pin
-// Applicable to STM, on MSP just set the PIN_SEL to 1 unless AF0 is passed
-// On STM if an AF is passed set PIN_MODER to 2 instead of IN or OUT, ANALOG
+// Applicable to STM, on MSP just set the PIN_SEL to 1 unless PIN_AF0 is passed
+// On STM if an PIN_AF is passed set PIN_MODER to 2 instead of IN or OUT, ANALOG
 // sets to 3
 typedef enum {
-  AF0 = 0; AF1; AF2; AF3; AF4; AF5; AF6; AF7;
-  ANALOG;  // Clever way to make DIR analog for STM as AF and Analog are
-           // mutually exclusive
-}
-PIN_AF;
+  PIN_AF_0 = 0,
+  PIN_AF_1,
+  PIN_AF_2,
+  PIN_AF_3,
+  PIN_AF_4,
+  PIN_AF_5,
+  PIN_AF_6,
+  PIN_AF_7,
+  PIN_AF_ANALOG,  // Clever way to make DIR analog for STM as PIN_AF and Analog
+                  // are mutually exclusive
+} PIN_AF;
 
 // Ignored settings:
 // Speed - should be set when a pin is initialized and not touched again
@@ -47,47 +58,25 @@ PIN_AF;
 // input or wired OR gates
 // Reset and Lock setting on STM
 
-// A struct to set the values for a pin
-typedef struct IOSettings {
-  const IOAddr addr;  // The necessity of making this const is debatable
-  PIN_DIR dir;
-  PIN_VAL val;
-  PIN_RES res;
-  PIN_AF alt;
-} IOSettings;
+// A 3 byte struct to set the values for a pin
+// Could separate out the address from the settings
+typedef struct GPIOSettings {
+  const GPIOAddress addr;  // 2 bytes
+  PIN_DIR dir;             // 1 bit
+  PIN_STATE state;         // 1 bit
+  PIN_RES res;             // 2 bits (one unused option)
+  PIN_AF alt;  // 4 bits (no padding needed if the compiler is smart about enums
+               // 7 unused options)
+} GPIOSettings;
 
-// Initializes a pin with all of the settings entered
-void gpio_init(IOSettings *settings);
+// Initializes GPIO globally, sets all pins to lowest power mode
+void gpio_init();
 
-// Updates the gpio based on the values passed in the struct
-// (just boilerplate for calling init again to make the user interface better
-// could maybe be avoid by calling gpio_set and gpio_unset instead of init and
-// deinit?)
-void gpio_update(IOSettings *settings);
+// Initializes a GPIO pin
+void gpio_init_pin(GPIOSettings *settings);
 
-// Sets the pin to the lowest power mode possible
-void gpio_deinit(IO_Addr *address);
-
-// Alternatively for toggles pass a IOSettings object and toggle the value in
-// the struct as well,
-// this may be confusing but also allows tracking of the settings of a given pin
-// nicely without
-// the need for functions to query the state of a pin. The solution presented
-// here assumes there
-// is very limited use cases for getting the value of a setting for a pin and
-// ignores it.
-
-// Toggles the direction of the pin
-void gpio_toggle_dir(IOAddr *address);
-
-// Toggles the output of the pin
-void gpio_toggle_val(IOAddr *address);
-
-// Sets the PUPDR (toggling this doesn't really make sense)
-void gpio_set_res(IOAddr *address, const PIN_RES res);
-
-// Sets the alt function (toggling this doesn't make sense)
-void gpio_set_af(IOAddr *address, const PIN_AF alt);
+// Toggles the output state of the pin
+void gpio_toggle_state(GPIOAddress *address);
 
 // Gets the value of the input register for a pin
-bool gpio_get_value(IOAddr *address);
+bool gpio_get_value(GPIOAddress *address);
