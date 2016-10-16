adef6a34b4e1e6b986aa59b92d5fa138e6ad46fb
Titus Chow <tituschow96@gmail.com>
2016-10-15 20:14:23 -0400

    Added rules.mk for fw build system compatibility

--- rules.mk
+++ rules.mk
@@ -5,10 +5,4 @@
 # $(LIB)_SRC: $(LIB)_DIR/src/*.c
 # $(LIB)_INC: $(LIB)_DIR/inc/*.h
 
-# Find all *.c files in root source and device-specific source folders.
-$(LIB)_SRC := $(wildcard $($(LIB)_SRC_ROOT)/*.c) \
-              $(wildcard $($(LIB)_SRC_ROOT)/$(PLATFORM)/*.c)
-
 $(LIB)_DEPS := $(PLATFORM_LIB)
-
-# Specifies library specific build flags
