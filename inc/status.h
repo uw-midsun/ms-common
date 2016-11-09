c9fa1f170a92cb06fdefd87b7ae2abc24f7b4ead
ckitagawa <calderk2010@gmail.com>
2016-11-08 21:54:05 -0500

    ELEC-41 created status library

---
+++ inc/status.h
@@ -0,0 +1,39 @@
+#pragma once
+// Status Library for more verbose error handling
+#include <string.h>
+
+// StatusCodes for various errors, keep these generic. Never assume their order is fixed so refer to
+// them by name only.
+typedef enum {
+  STATUS_CODE_OK = 0,
+  STATUS_CODE_UNKNOWN,
+  STATUS_CODE_INVALID_ARGS,
+  STATUS_CODE_RESOURCE_EXHAUSTED,
+  STATUS_CODE_UNREACHABLE,
+  STATUS_CODE_TIMEOUT,
+  STATUS_CODE_EMPTY,
+  STATUS_CODE_OUT_OF_RANGE,
+  STATUS_CODE_UNIMPLEMENTED,
+  NUM_STATUS_CODE,
+} StatusCode;
+
+#define STATUS_MESSAGE_SIZE 48
+
+typedef struct Status {
+  StatusCode code;
+  char message[STATUS_MESSAGE_SIZE];
+} Status;
+
+// Creates a status struct containing an error code and optionally a message.
+Status status_create(const StatusCode code, char* message, const int length);
+
+// A macro for returning with just an error code.
+#define status(code) status_create(code, "", 0)
+
+// A cleaner way to define a status with a message.
+#define status_msg(code, message) status_create(code, message, strlen(message))
+
+// A macro for checking if a status is OK please use this as it is much cleaner and a bit shorter.
+#define status_ok(status) status.code == STATUS_CODE_OK
+
+// TODO(ELEC-41): Add support for dumping error STDOUT/STDERR or CAN for debugging.
---
+++ test/test_status.c
@@ -0,0 +1,50 @@
+#include "status.h"
+
+#include "misc.h"
+#include "unity.h"
+
+void setup_test(void) {}
+
+void teardown_test(void) {}
+
+// status_create
+
+// Verifies a few common use cases work.
+void test_status_create_valid(void) {
+  // Most common use case
+  Status status = status(STATUS_CODE_UNIMPLEMENTED);
+  TEST_ASSERT_EQUAL(STATUS_CODE_UNIMPLEMENTED, status.code);
+  TEST_ASSERT_EQUAL_STRING("", status.message);
+
+  // Most common verbose use case.
+  status = status_msg(STATUS_CODE_EMPTY, "Nothing here.");
+  TEST_ASSERT_EQUAL(STATUS_CODE_EMPTY, status.code);
+  TEST_ASSERT_EQUAL_STRING("Nothing here.", status.message);
+
+  // Second most common verbose use case.
+  char mystery[] = "It's a mystery";
+  status = status_create(STATUS_CODE_UNKNOWN, mystery, SIZEOF_ARRAY(mystery));
+  TEST_ASSERT_EQUAL(STATUS_CODE_UNKNOWN, status.code);
+  TEST_ASSERT_EQUAL_STRING(mystery, status.message);
+}
+
+// Verifies the behavior when invalid args are entered.
+void test_status_create_invalid_args(void) {
+  Status status =
+      status_msg(STATUS_CODE_TIMEOUT, "Took toooooooooooooooooooooooooooooooooooooo long");
+  TEST_ASSERT_EQUAL(STATUS_CODE_INVALID_ARGS, status.code);
+  TEST_ASSERT_EQUAL_STRING("message too long", status.message);
+
+  status = status_msg(NUM_STATUS_CODE, "message");
+  TEST_ASSERT_EQUAL(STATUS_CODE_INVALID_ARGS, status.code);
+  TEST_ASSERT_EQUAL_STRING("code invalid", status.message);
+}
+
+// Verifies the status ok macro works.
+void test_status_ok_macro(void) {
+  Status status = status(STATUS_CODE_OK);
+  TEST_ASSERT_TRUE(status_ok(status));
+
+  status = status(STATUS_CODE_TIMEOUT);
+  TEST_ASSERT_FALSE(status_ok(status));
+}
